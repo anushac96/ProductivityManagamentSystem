256c79592680f374f672549c673094734a6e8b66
anushac96 <anushac96@gmail.com>
2024-01-03 14:12:08 +0530

    Pomodaro timer added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function Timer({ setTimerState }) {
+  const [time, setTime] = useState(1500);
+  const [timerRunning, setTimerRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (timerRunning) {
+      interval = setInterval(() => {
+        if (time === 0) {
+          clearInterval(interval);
+          setTimerRunning(false);
+          setTimerState('break');
+        } else {
+          setTime((prevTime) => prevTime - 1);
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerRunning, time, setTimerState]);
+
+  const startTimer = () => {
+    if (!timerRunning) {
+      setTimerRunning(true);
+    }
+  };
+
+  const pauseTimer = () => {
+    setTimerRunning(false);
+  };
+
+  const resetTimer = () => {
+    setTime(1500);
+    setTimerRunning(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <h1>Timer</h1>
+      <h2>
+        {Math.floor(time / 60)}:{time % 60 < 10 ? '0' : ''}
+        {time % 60} mins
+      </h2>
+      {!timerRunning && <button onClick={startTimer}>Start</button>}
+      {timerRunning && <button onClick={pauseTimer}>Pause</button>}
+      <button onClick={resetTimer}>Reset</button>
     </div>
   );
 }
 
-export default App;
+export default Timer;
